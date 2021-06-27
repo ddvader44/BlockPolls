71ba342fa5f1ca2603350d88a7d4ea71a5c4d8ab
Dishit <40226856+ddvader44@users.noreply.github.com>
2021-06-28 02:18:18 +0530

    polling, jquery error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PollCreateComponent } from './poll-create/poll-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PollCreateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/poll-create/poll-create.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-poll-create',
+  templateUrl: './poll-create.component.html',
+  styleUrls: ['./poll-create.component.scss']
+})
+export class PollCreateComponent {
+
+  pollForm: FormGroup; // group storing inputs
+
+  constructor(private fb: FormBuilder) {
+
+    this.pollForm = this.fb.group({
+      question: this.fb.control('',[Validators.required]), //first parameter, initialized by empty string and validated(required or not)
+      image: this.fb.control(''), // no need of validation, you can choose to leave it empty
+      op1: this.fb.control(''),
+      op2: this.fb.control(''),
+      op3: this.fb.control(''),
+    });
+   }
+
+   submitForm(){
+     console.log(this.pollForm.value)
+   }
+
+}
