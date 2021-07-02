0cc3ebad47918a47a6b2451393e93c67acafc1a4
Dishit <40226856+ddvader44@users.noreply.github.com>
2021-07-03 00:55:11 +0530

    updated options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,13 +10,13 @@ export class AppComponent {
 
   polls = [
     {
-    question : "What do you like more, Pizza or Burger?",
+    question : "What do you like more, Pizza or Burger or Pasta",
     image : "https://images.unsplash.com/photo-1571407970349-bc81e7e96d47?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=625&q=80",
-    votes: [4,5,0],
+    votes: [4,5,1],
     voted : true,
   },
   {
-    question : "Reservoir Dogs or OUATIH or Pulp Fiction",
+    question : "Reservoir Dogs or Kill Bill or Pulp Fiction",
     image : "https://images.unsplash.com/photo-1594073753319-df2a78cfd4e7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
     votes: [100,10,10],
     voted : false,
