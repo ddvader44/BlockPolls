e5b2f17f1e776101892c314068fe2afb14926801
Dishit <40226856+ddvader44@users.noreply.github.com>
2021-07-03 23:20:00 +0530

    apexchart

--- src/app/poll-vote/poll-vote.component.ts
+++ src/app/poll-vote/poll-vote.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import ApexCharts from 'apexcharts';
 
 @Component({
   selector: 'app-poll-vote',
@@ -8,7 +9,11 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class PollVoteComponent implements OnInit {
 
-  options = ["Pizza","Burger","Pasta"];
+  @Input() voted: boolean;
+  @Input() options: string[];
+  @Input() results: number[];
+  @Input() question : string;
+
   voteForm : FormGroup;
 
   constructor(private fb : FormBuilder) {
@@ -18,10 +23,42 @@ export class PollVoteComponent implements OnInit {
    }
 
   ngOnInit(): void {
+    if(this.voted){
+      this.generateChart();
+    }
   }
 
   submitForm(){
     console.log(this.voteForm.value);
   }
 
+  generateChart(){
+    const options : ApexCharts.ApexOptions = {
+      series: [
+        {
+        data: this.results,
+      },
+    ],
+      chart: {
+        height:350,
+        type: 'bar',
+      },
+      plotOptions : {
+        bar : {
+          columnWidth : "45%",
+          distributed : true,
+        },
+      },
+      legend : {
+        show : false,
+      },
+      xaxis: {
+        categories: this.options,
+      },
+    };
+
+    const chart = new ApexCharts(document.querySelector("poll-results"), options);
+    chart.render();
+
+  }
 }
