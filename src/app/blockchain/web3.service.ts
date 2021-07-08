bbb55836389746f716e82efc4309632d04cec338
Dishit <40226856+ddvader44@users.noreply.github.com>
2021-07-09 00:21:01 +0530

    web3js

---
+++ src/app/blockchain/web3.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import Web3 from 'web3';
+import { Contract } from 'web3-eth-contract';
+
+declare var window : any;
+const contractAbi = require("./contractABI.json");
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Web3Service {
+
+  private web3 : Web3;
+  private contract : Contract;
+  private contractAddress = "0x...."; // replace with your local blockchain created contract Address from Ganache Transactions
+  
+  constructor()
+  {
+    if(window.web3) {
+
+      this.web3 = new Web3(window.ethereum);
+      this.contract = new this.web3.eth.Contract(
+        contractAbi,
+        this.contractAddress
+      );
+
+      window.ethereum.enable().catch((err) =>  {
+        console.log(err);
+      });
+    }
+    else {
+      console.warn("Metamask not found!");
+    }
+  }
+
+}
